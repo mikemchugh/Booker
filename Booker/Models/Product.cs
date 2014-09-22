fc1aa7437d7a8964acaaa63fce0893bb5ee78244
Mike McHugh <mike.a.mchugh@gmail.com>
2014-09-22 20:35:24 +0100

    Initial commit

---
+++ Booker/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Booker.Models
+{
+    public class Product
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public double Cost { get; set; }
+    }
+}
---
+++ Booker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Booker.Startup))]
+namespace Booker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
